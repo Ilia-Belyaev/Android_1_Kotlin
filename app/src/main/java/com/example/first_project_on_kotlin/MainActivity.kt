529aefe20663c84f5dde15ffb418ce1dbac1d5b1
Ilia Belyaev <ilya.belyaev.95@list.ru>
2021-07-01 09:26:21 +0300

    Конвертировал java в kotlin

---
+++ app/src/main/java/com/example/first_project_on_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.first_project_on_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.first_project_on_kotlin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
